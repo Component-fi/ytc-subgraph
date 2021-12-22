5ec49160ef464dfb53b8786bf86ffd73898a4a48
Mattias Lightstone <mattias.lightstone@gmail.com>
2021-12-22 11:22:15 -0500

    Initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,3 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import { YieldTokenCompounding } from "../generated/YieldTokenCompounding/YieldTokenCompounding"
+import { ExampleEntity } from "../generated/schema"
